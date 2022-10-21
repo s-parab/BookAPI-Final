ba38faef61b8a8945cbb6c27a12ee43ae540f0d4
Shivam Parab <shivam.parab@wonderbiz.in>
2022-10-21 11:35:13 +0530

    Updates.

--- BookAPI/Models/Book.cs
+++ BookAPI/Models/Book.cs
@@ -3,9 +3,9 @@
     public class Book
     {
         public Guid Id { get; set; }
-        public string? Title { get; set; }
+        public string Title { get; set; } = string.Empty;
 
-        public string? Author { get; set; }
+        public string Author { get; set; } = String.Empty;
 
         public DateTime PublishDate { get; set; }
     }
